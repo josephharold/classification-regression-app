6af1906b3bc3c26a786095ea747c88d54fbf17c6
Joseph Harold <josephharold.timbang.dev@gmail.com>
2023-01-11 17:07:48 +0800

    initialize app

---
+++ rcProject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rcProject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rcProject.settings")
+
+application = get_wsgi_application()
---
+++ src/js/bundle.js
@@ -0,0 +1,4 @@
+
+import '../styles/main.css'
+const container = document.getElementById('container');
+container.innerHTML = 'helloworld';
