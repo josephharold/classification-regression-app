ba682dcd6a50eee83c623642fa9980a0923e2f37
Joseph Harold <josephharold.timbang.dev@gmail.com>
2023-01-11 19:24:51 +0800

    add & integrate getCPrediction API

--- rcApp/views.py
+++ rcApp/views.py
@@ -1,7 +1,48 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+import matplotlib.pyplot as plt
+import joblib
+import pandas as pd
 
+classificationModel = joblib.load('D:\School\\4th Year\ITD105\ITD105-FinalProj\webapp\\rcProject\\rcApp\\model.aiml')
+classficationPred = {0: 'water isPotabble', 1: 'water !isPotable'}
 
+@csrf_exempt
+def getCPrediction(request):
+	if(request.method=="POST"):
+		ph = request.POST.get('ph', 'default')
+		hardness = request.POST.get('hardness', 'default')
+		solids = request.POST.get('solids', 'default')
+		chloramines = request.POST.get('chloramines', 'default')
+		sulfate = request.POST.get('sulfate', 'default')
+		conductivity = request.POST.get('conductivity', 'default')
+		organic_carbon = request.POST.get('organic_carbon', 'default')
+		trihalomethanes = request.POST.get('trihalomethanes', 'default')
+		turbidity = request.POST.get('turbidity', 'default')
+
+		ph = float(ph)
+		hardness = float(hardness)
+		solids = float(solids)
+		chloramines = float(chloramines)
+		sulfate = float(sulfate)
+		conductivity = float(conductivity)
+		organic_carbon = float(organic_carbon)
+		trihalomethanes = float(trihalomethanes)
+		turbidity = float(turbidity)
+
+		inputs = [[ph, hardness, solids, chloramines, sulfate, conductivity, organic_carbon, trihalomethanes, turbidity]]
+		predicted = classificationModel.predict(inputs)
+		predicted = classficationPred[predicted[0]]
+	
+		res = {'prediction': predicted}	
+		return JsonResponse(res, safe=False)
+	dct = {}
+	dct['data'] = 'data'
+	return JsonResponse(dct , safe=False)
+
+@csrf_exempt
 def index(request):
 	return render(request, 'index.html')
+
--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,4 +1,34 @@
-
 import '../styles/main.css'
-const container = document.getElementById('container');
-container.innerHTML = 'helloworld';
+import axios from 'axios';
+// const container = document.getElementById('container');
+// container.innerHTML = 'helloworld';
+
+const item1 = document.getElementById("item1");
+const item2 = document.getElementById("item2");
+const item3 = document.getElementById("item3");
+
+const fetchCPrediction = async ()=>{
+	event.preventDefault()
+    
+	let data = new FormData(); // 2
+	
+	data.append("ph", document.getElementById('ph').value)  
+	data.append("hardness", document.getElementById('hardness').value)  
+	data.append("solids", document.getElementById('solids').value)  
+	data.append("chloramines", document.getElementById('chloramines').value)  
+	data.append("sulfate", document.getElementById('sulfate').value)  
+	data.append("conductivity", document.getElementById('conductivity').value)  
+	data.append("organic_carbon", document.getElementById('organic_carbon').value)  
+	data.append("trihalomethanes", document.getElementById('trihalomethanes').value)  
+	data.append("turbidity", document.getElementById('turbidity').value)  
+	data.append("csrfmiddlewaretoken", '{{csrf_token}}') // 3
+	
+	// axios.post('create_note/', data) // 4
+	//  .then(res => alert("Form Submitted")) // 5
+	//  .catch(errors => console.log(errors)) // 6
+	axios.post('http://127.0.0.1:8000/getCPrediction', data)
+		.then(res=> console.log(res.data))
+		.catch(err=>console.log(err))
+}
+const classificationButton = document.getElementById("classificationButton")
+classificationButton.addEventListener('click', ()=>{fetchCPrediction()})
